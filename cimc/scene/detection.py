64fa79b3aefe89f73d73830748688ac05318ed5d
quartz55 <john.razor97@gmail.com>
2018-05-20 17:17:15 +0100

    Basic scene detection (needs optimizations)

--- cimc/scene/detection.py
+++ cimc/scene/detection.py
@@ -1,23 +1,106 @@
-import scenedetect as sd
-from scenedetect.detectors import ContentDetector
-from scenedetect.manager import SceneManager
+from typing import Optional, Tuple
+
+import cv2
+import numpy as np
+from numba import jit
 
 from cimc import resources
 
 
-class SceneDetector:
-    def __init__(self, threshold=0.3, min_length=15, downscale=1):
-        self.downscale = downscale
-        self._detector = ContentDetector(threshold=threshold, min_scene_len=min_length)
+#@jit()
+def is_cut(frames: np.ndarray, threshold: float = 30.0) -> bool:
+    assert frames.shape[0] >= 2, 'Needs at least 2 frames'
+    frame_1, frame_2 = frames[[0, -1]]  # first and last fame
+    assert frame_1.shape == frame_2.shape, "Frames need to be the same size"
+    frame_1_hsv = cv2.split(cv2.cvtColor(frame_1, cv2.COLOR_RGB2HSV))
+    frame_2_hsv = cv2.split(cv2.cvtColor(frame_2, cv2.COLOR_RGB2HSV))
 
-    def __call__(self, video_uri: str):
-        smgr = SceneManager(detector=self._detector,
-                            downscale_factor=self.downscale)
-        sd.detect_scenes_file(video_uri, smgr)
-        return smgr.scene_list
+    delta_hsv = np.array([-1, -1, -1], dtype=np.float64)
+    for i in range(3):
+        num_pixels = frame_2_hsv[i].shape[0] * frame_2_hsv[i].shape[1]
+        frame_1_hsv[i] = frame_1_hsv[i].astype(np.int32)
+        frame_2_hsv[i] = frame_2_hsv[i].astype(np.int32)
+        delta_hsv[i] = np.sum(np.abs(frame_2_hsv[i] - frame_1_hsv[i])) / float(num_pixels)
+    delta_h, delta_s, delta_v = delta_hsv
+    delta_hsv_avg = sum(delta_hsv) / 3.0
+
+    return delta_hsv_avg >= threshold
+
+
+class SceneDetector:
+    def __init__(self, threshold=30.0, min_length=15, downscale=1, skip=1):
+        assert 0 < threshold < 100
+        assert min_length > 0
+        assert downscale >= 1
+        self.threshold = threshold
+        self.min_length = min_length
+        self.skip = skip
+        self.downscale_factor = downscale
+        self._curr_id = 0
+        self._last_frame = None
+        self._last_scene = 0
+        self._skip_to = 0
+
+    def __call__(self, frame: np.ndarray):
+        return self.update(frame)
+
+    def update(self, frame: np.ndarray) -> bool:
+        cut = False
+        if self._curr_id == self._skip_to:
+            self._skip_to = self._curr_id + self.skip
+            if self._curr_id == 0:
+                cut = True
+                self._last_scene = -self.min_length
+            elif self._last_frame is not None:
+                f1 = self.downscale(self._last_frame, self.downscale_factor)
+                f2 = self.downscale(frame, self.downscale_factor)
+                if is_cut(np.array([f1, f2]), self.threshold):
+                    cut = True
+            self._last_frame = frame
+        if cut and self._curr_id - self._last_scene >= self.min_length:
+            self._last_scene = self._curr_id
+        else:
+            cut = False
+        self._curr_id += 1
+        return cut
+
+    @staticmethod
+    def downscale(frame: np.ndarray, factor: int = 1) -> np.ndarray:
+        return frame[::factor, ::factor, :]
+
+
+def detect(video_uri: str):
+    import imageio
+    import os.path
+    from tqdm import tqdm
+    detector = SceneDetector(downscale=4, skip=1)
+    scenes = []
+    bleep_cut = 0
+    with imageio.get_reader(video_uri) as video:
+        fps = video.get_meta_data()['fps']
+        out_uri, ext = os.path.splitext(video_uri)
+        out_uri = f"{out_uri}-scenes.{ext}"
+        with imageio.get_writer(out_uri,
+                                fps=fps,
+                                quality=5) as out:
+            bar = tqdm(total=len(video))
+            for i, frame in enumerate(video):
+                bar.update()
+                if detector.update(frame):
+                    bleep_cut += int(fps / 4)
+                    bar.set_postfix(last_scene=i)
+                    scenes.append(i)
+                out_frame = frame
+                if bleep_cut > 0:
+                    out_frame[-100:, 100:] = [255, 0, 0]
+                    bleep_cut -= 1
+                out.append_data(out_frame)
+            bar.close()
+    return scenes
 
 
 if __name__ == '__main__':
-    detector = SceneDetector(downscale=4)
-    scenes = detector(resources.video('goldeneye.mp4'))
+    scenes = detect(resources.video('goldeneye.mp4'))
     print(scenes)
+    scene_ranges = list(zip(scenes[:-1], scenes[1:]))
+    print(scene_ranges)
